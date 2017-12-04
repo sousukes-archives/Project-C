c54321af005bd4605f66d99bf0cae8e470b4cdc3
Kamesuta <kamesuta@gmail.com>
2017-12-05 01:04:27 +0900

    Fix #1 mcmod.infoの問題の修正

--- src/main/java/sousuke/sousukeMod_main/Sousuke_sMod.java
+++ src/main/java/sousuke/sousukeMod_main/Sousuke_sMod.java
@@ -3,10 +3,10 @@ package sousuke.sousukeMod_main;
 import cpw.mods.fml.common.Mod;
 
 
-@Mod(modid = "Sousuke_sMod_core",name = "sousuke's Mod core",version = "rc0 alpha")
+@Mod(modid = Sousuke_sMod.MODID,name = "sousuke's Mod core",version = "rc0 alpha")
 public class Sousuke_sMod
 {
-    public static final String MODID = "";
-    
-    
+    public static final String MODID = "sousuke_s_mod_core";
+
+
 }
