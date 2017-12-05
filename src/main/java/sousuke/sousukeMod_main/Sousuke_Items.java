39f182ca8a103a5d4937b90ff058e14ce198d5b8
sousuke0422 <33174568+sousuke0422@users.noreply.github.com>
2017-12-05 19:14:01 +0900

    かめすた氏によるfix

--- src/main/java/sousuke/sousukeMod_main/Sousuke_Items.java
+++ src/main/java/sousuke/sousukeMod_main/Sousuke_Items.java
@@ -7,42 +7,42 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
-@Mod(modid = "Sousuke_sMod_Items",name = "sousuke's Mod Items",version = "rc0 alpha")
+@Mod(modid = "sousuke_s_mod_items",name = "sousuke's Mod Items",version = "rc0 alpha")
 
 public class Sousuke_Items {
-	
+
 	//変数の定義
-	
+
 		public static Item ruby;
-		
+
 		public static Item ruby_dust;
-		
+
 		public static Item Iridium_alloy;
-		
-		
+
+
 		//アイテム追加
 		@EventHandler
-		
+
 		public static void preInti(FMLPreInitializationEvent event) {
-			
+
 			ruby = new Item()
 		    		 .setUnlocalizedName("ruby")
 		    		 .setCreativeTab(CreativeTabs.tabMaterials)
 		    		 .setTextureName("ruby");
 		     GameRegistry.registerItem(ruby, "ruby");
-		     
+
 		     ruby_dust = new Item()
 		    		 .setUnlocalizedName("ruby_dust")
 		    		 .setCreativeTab(CreativeTabs.tabMaterials)
 		    		 .setTextureName("sousuke:ruby_dust");
 			GameRegistry.registerItem(ruby_dust, "ruby dust");
-			
+
 			Iridium_alloy = new Item()
 					.setUnlocalizedName("Iridium_alloy")
 					.setCreativeTab(CreativeTabs.tabMaterials)
 					.setTextureName("sousuke:Iridium_alloy");
 			GameRegistry.registerItem(Iridium_alloy, "Iridium alloy");
-			
+
 		}
 
 }
--- src/main/java/sousuke/sousukeMod_main/Sousuke_Recipes.java
+++ src/main/java/sousuke/sousukeMod_main/Sousuke_Recipes.java
@@ -2,9 +2,9 @@ package sousuke.sousukeMod_main;
 
 import cpw.mods.fml.common.Mod;
 
-@Mod(modid = "Sousuke_sMod_recipes",name = "sousuke's Mod recipes",version = "rc0 alpha")
+@Mod(modid = "sousuke_s_mod_recipes",name = "sousuke's Mod recipes",version = "rc0 alpha")
 
 public class Sousuke_Recipes {
-	
+
 
 }
