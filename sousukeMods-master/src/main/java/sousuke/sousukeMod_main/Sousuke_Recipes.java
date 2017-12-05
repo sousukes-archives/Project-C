bce63f671743cd14f287e1a00ac0f5766ae72097
sousuke0422 <33174568+sousuke0422@users.noreply.github.com>
2017-12-05 19:02:00 +0900

    Add files via upload

---
+++ sousukeMods-master/src/main/java/sousuke/sousukeMod_main/Sousuke_Recipes.java
@@ -0,0 +1,10 @@
+package sousuke.sousukeMod_main;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "sousuke_s_mod_recipes",name = "sousuke's Mod recipes",version = "rc0 alpha")
+
+public class Sousuke_Recipes {
+
+
+}
